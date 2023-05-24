94c41b5622ac348ab2986ff54bc462fd793bf5d9
Kevin <95513702+TravelerSW@users.noreply.github.com>
2023-05-24 12:19:13 -0300

    Create operacao.java

---
+++ operacao.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class operacao{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quer dar inicio? 1 para sim e 2 para não\n");
+        int res = scanner.nextInt();
+        
+        if(res !=1){
+          scanner.close();
+          System.exit(res);  
+        }
+
+        do{
+            System.out.println("Insira um dividendo: ");
+            int dividendo = scanner.nextInt();
+            System.out.println("Insira um divisor");
+            int divisor = scanner.nextInt();
+               try{ 
+                    int resul = dividendo / divisor;
+                    System.out.println("O resultado da operação é: " + resul);
+                    System.out.println("Continuar? ");
+                    res = scanner.nextInt();
+
+                    
+               }catch(ArithmeticException e){
+                System.out.println("Não pode ser divido por zero");
+               }
+        } while(res == 1);
+
+    }
+}
